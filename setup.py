c2117e282dbeefe57f1b7f4c433ea752ae6f73e7
Lincoln Puzey <lincoln@puzey.dev>
2020-06-06 12:45:01 +0800

    Moved to pre-alpha development status

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setuptools.setup(
     license="MIT License",
     packages=["fourth"],
     classifiers=[
-        "Development Status :: 1 - Planning",
+        "Development Status :: 2 - Pre-Alpha",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
         "Topic :: Software Development :: Libraries :: Python Modules",
